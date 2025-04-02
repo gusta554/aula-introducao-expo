0cbbb7fea8fd77fadd103bd864d12fa9bd3d8e54
gusta554 <ykonno26@gmail.com>
2025-04-02 20:39:56 -0300

    aula02-04

--- src/exemplos/ex-03/index.js
+++ src/exemplos/ex-03/index.js
@@ -1,20 +1,42 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Alert, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-function Exemplo3 () {
-    return(
+function Exemplo3() {  
+
+    function handleExibeMensagem () {
+        Alert.alert('Teste')
+    }
+
+    const handleOutroJeitoFuncao = () => {
+        Alert.alert('Título', 'Texto mensagem', [
+            {
+              text: 'Meu texto',
+              onPress: () => Alert.alert('Tchau'),
+              style: 'default',
+            },
+          ])
+    }
+
+    return (
         <View style={styles.container}>
-                <Text style={styles.titulo}>Exemplo 3</Text>
-                <Text style={styles.texto}>variáveis vs states</Text>
-                <Button
-                onPress={() => {}}
-                title='alerta'
-                color='deepskyblue'
-                accessibilityLabel='Mensaagem de alerta'
-                />
+            <Text style={styles.titulo}>Exemplo 3</Text>
+            <Text style={styles.texto}>Variáveis vs State</Text>
+            <View style={styles.botaoAlert}>
+            <Button
+                onPress={handleOutroJeitoFuncao}
+                title="Alerta"
+                color="red"
+                accessibilityLabel="Mensagem de alerta"                  
+            />
+        </View>
+        <Text style={styles.txtBotao}>Valor</Text>
+        <TouchableOpacity style={styles.botaoAlert}>
+            <Text style={styles.numero}>Adicionar +1</Text>
+        </TouchableOpacity>
         </View>
     );
 }
 
 export default Exemplo3;
+
--- src/exemplos/ex-03/styles.js
+++ src/exemplos/ex-03/styles.js
@@ -1,34 +1,52 @@
-import {  StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'; 
 import { RFPercentage } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#fff',
-        padding: 8,
-        flex: 1,
-        alignItems: 'center',
+        backgroundColor: '#fff', 
+        padding: 8, 
+        flex: 1, 
+        alignItems: 'center', 
         borderRadius: 20,
-    },
+    }, 
     titulo: {
-        fontSize: RFPercentage(3),
-        fontWeight: 'bold',
-        borderWidth: 2,
-        borderRadius: 15,
-        borderColor: 'deepskyblue',
-        marginTop: 10,
-        marginBottom: 10,
-        padding: 8,
-        width: '100%',
-        height: 'center',
-        textAlign: 'center',
-        textAlignVertical: 'center',
-       
-    },
+        fontSize: RFPercentage(3), 
+        color: 'deepskyblue', 
+        fontWeight: 'bold', 
+        borderWidth: 2, 
+        borderColor: 'deepskyblue', 
+        marginTop: 10, 
+        marginBottom: 10, 
+        padding: 8, 
+        width: '100%', 
+        height: 80, 
+        textAlign: 'center', 
+        textAlignVertical: 'center', 
+        borderRadius: 20,
+    }, 
     texto: {
-        fontSize: RFPercentage(2.2),
-        color: 'deepskyblue',
+        fontSize: RFPercentage(2.2),  
+    }, 
+    botaoAlert: {
+       width: '100%',
+       margin: RFPercentage(1),
+         padding: RFPercentage(1),
+            backgroundColor: 'red',
+            borderRadius: 10,
+      
     },
-    
+    numero: {
+        fontSize: RFPercentage(2.5),
+        color: '#fafafa',
+        fontWeight: 'bold',
+        
+    },
+    txtBotao: {
+        fontSize: RFPercentage(2.5),
+        color: 'purple',
+        fontWeight: 'bold',
+    },
 });
 
 export default styles;
+
