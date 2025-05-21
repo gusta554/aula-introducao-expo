3612e7274d3670da0b29077524d333ff9bfee6fe
gusta554 <ykonno26@gmail.com>
2025-05-21 20:38:36 -0300

    aula dia 21-05

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Exemplo3 from './src/exemplos/ex-03';
 import Exemplo4 from './src/exemplos/ex-04';
 import Exemplo5 from './src/exemplos/ex-05';
 import Exemplo6 from './src/exemplos/ex-06';
+import Exemplo7 from './src/exemplos/ex-07';
 
 
 //atividade 
@@ -18,7 +19,8 @@ import Atividade2 from './src/atividades/atividade-02';
 import Atividade3 from './src/atividades/atividade-03';
 import Atividade4 from './src/atividades/atividade-04';
 import Atividade5 from './src/atividades/atividade-05';
-import Atividade6 from './src/atividades/atividade-06';
+import Atividade6 from './src/atividades/atividade-06'; 
+import Atividade7 from './src/atividades/atividade-07';
 
 
 
@@ -26,7 +28,7 @@ import Atividade6 from './src/atividades/atividade-06';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade6/>
+      <Atividade7/>
       <StatusBar style="ligth" />
     </View>
   );
